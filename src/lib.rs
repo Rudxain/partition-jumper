f4f247bd712c2e5c02d2f93dbaf505eef634bb7f
Rudxain <76864299+Rudxain@users.noreply.github.com>
2025-12-20 01:36:34 -0400

    add witness-tracking ref-impls (draft)

--- src/lib.rs
+++ src/lib.rs
@@ -493,6 +493,127 @@ pub fn fsum_s(s: &[f64]) -> f64 {
 	acc
 }
 
+// TO-DO: translate from Python
+/*
+// single-witness tracking.
+// NOT TESTED
+pub fn isum_1w(s: &[u8]) -> usize {
+	let mut acc = 0;
+	let mut cur_part_start = 0;
+	let mut cur_windex: Option<usize> = None;
+	let mut cur_witness: Option<u8> = None;
+	let mut next_windex: Option<usize> = None;
+	let mut next_witness: Option<u8> = None;
+	let l  = s.len();
+	while cur_part_start < l:
+		target = s[cur_part_start]
+		// begin exp-s:
+		pow2 = 2
+		if cur_windex is not None:
+			assert target == cur_witness
+			assert cur_part_start <= cur_windex
+		hi = cur_part_start + pow2 if cur_windex is None else cur_windex + 1
+		while True:
+			if hi > l:
+				hi = l // clamp
+				break
+			if target != s[hi - 1]:
+				next_windex = hi - 1
+				next_witness = s[next_windex]
+				break
+			pow2 <<= 1
+			hi = cur_part_start + pow2
+		// end exp-s.
+		// t := cur_part_start + (pow2 >> 1) - 1,
+		// w := cur_windex,
+		// invariants:
+		// `w < t` iff `pow2 > 2`
+		// `t <= w` if `pow2 == 2`
+		lo = cur_part_start + (pow2 >> 1) - 1 if cur_windex is None or pow2 > 2 else cur_windex
+		assert lo >= 0
+		// begin bin-s:
+		if target == s[hi - 1]:
+			next_part_start = hi
+		else:
+			if next_windex is not None:
+				assert next_witness is not None
+				// if the new witness is closer to the target,
+				// then it's more likely to belong to an adjacent partition
+				if hi - 1 < next_windex and s[hi-1] != next_witness:
+					next_windex = hi - 1
+					next_witness = s[next_windex]
+				// dupe loop, to reduce branching.
+				// TO-DO: merge loops, because optimizer already dupes
+				while lo < hi:
+					mid = (lo + hi) >> 1
+					if target != s[mid]:
+						hi = mid
+						assert hi != 0
+						if s[hi] != next_witness:
+							next_windex = hi
+							next_witness = s[next_windex]
+					else:
+						lo = mid + 1
+			while lo < hi:
+				mid = (lo + hi) >> 1
+				if target != s[mid]:
+					hi = mid
+				else:
+					lo = mid + 1
+			next_part_start = lo
+		// end bin-s.
+		run_len = next_part_start - cur_part_start
+		acc += run_len * target
+		cur_part_start = next_part_start
+		cur_windex = next_windex
+		cur_witness = next_witness
+		// cleanup just-in-case
+		next_windex = None
+		next_witness = None
+	acc
+}
+
+// multi-witness tracking.
+// This uses auxiliary memory proportional to the number of partitions.
+pub fn isum_h(s: &[u8]) -> usize {
+	acc = 0
+	cur_part_start = 0
+	// tracks the maximum known range of each partition,
+	// which happens to be its minimum size
+	part_ranges: dict[int, tuple[int, int]] = {}
+	while cur_part_start < len(s):
+		target = s[cur_part_start]
+		if target == s[-1]:
+			run_len = len(s) - cur_part_start
+			acc += run_len * target
+			return acc
+		lo = cur_part_start + 1
+		// TO-DO: use map to reduce this
+		hi = len(s)
+		while lo < hi:
+			mid = (lo + hi) // 2
+			m = s[mid]
+			pr = part_ranges.get(m)
+			if pr is None:
+				part_ranges[m] = (mid, mid+1)
+			else:
+				if mid < pr[0]:
+					part_ranges[m] = (mid, pr[1])
+				// WARN: off by 1?
+				if mid >= pr[1]:
+					part_ranges[m] = (pr[0], mid)
+			if target != m:
+				hi = mid
+			else:
+				lo = mid + 1
+		next_part_start = lo
+		run_len = next_part_start - cur_part_start
+		acc += run_len * target
+		cur_part_start = next_part_start
+	acc
+}
+*/
+
 #[cfg(test)]
 mod tests {
 	use super::*;
